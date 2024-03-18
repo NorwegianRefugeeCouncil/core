18ada6931889801b2732f5e0044907707b5366c9
David Figueroa <david.figueroa@nrc.no>
2024-03-18 12:58:20 +0100

    [CORE24-113] fix(e2e): automatically start and stop api for running tests

--- apps/core-api-e2e/src/support/global-setup.ts
+++ apps/core-api-e2e/src/support/global-setup.ts
@@ -1,9 +1,20 @@
 /* eslint-disable */
+import { spawn } from 'child_process';
+
+import waitOn from 'wait-on';
+
 var __TEARDOWN_MESSAGE__: string;
 
 module.exports = async function () {
-  // Start services that that the app needs to run (e.g. database, docker-compose, etc.).
+  // Start services that the app needs to run (e.g. database, docker-compose, etc.).
   console.log('\nSetting up...\n');
+
+  // Start the server
+  global.__SERVER__ = spawn('nx', ['run', 'core-api:serve'], {
+    detached: true,
+  });
+  // Wait for the server to be ready
+  await waitOn({ resources: ['http-get://localhost:3333/api'] });
 
   // Hint: Use `globalThis` to pass variables to global teardown.
   globalThis.__TEARDOWN_MESSAGE__ = '\nTearing down...\n';
