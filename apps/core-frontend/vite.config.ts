4b78f3176c804edeaed3228c09f3249520fb7c6d
Ben McAlindin <bmcalindin@gmail.com>
2024-03-20 11:01:12 +0100

    [CORE24-114] task(frontend): Fix vite config for react-native-web

--- apps/core-frontend/vite.config.ts
+++ apps/core-frontend/vite.config.ts
@@ -3,6 +3,20 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
+const extensions = [
+  '.web.tsx',
+  '.tsx',
+  '.web.ts',
+  '.ts',
+  '.web.jsx',
+  '.jsx',
+  '.web.js',
+  '.js',
+  '.css',
+  '.json',
+  '.mjs',
+];
+
 export default defineConfig({
   root: __dirname,
   cacheDir: '../../node_modules/.vite/apps/core-frontend',
@@ -32,19 +46,19 @@ export default defineConfig({
     },
     // rollupOptions: {
     //   external: [
-    //     "react",
-    //     "jsx-runtime",
-    //     "react-dom",
-    //     "react-native",
-    //     "react-native-svg",
+    //     'react',
+    //     'jsx-runtime',
+    //     'react-dom',
+    //     'react-native',
+    //     'react-native-svg',
     //     // "react/jsx-runtime",
     //   ],
     //   output: {
     //     globals: {
-    //       react: "React",
-    //       "jsx-runtime": "jsxRuntime",
-    //       "react-native": "ReactNative",
-    //       "react-dom": "ReactDOM",
+    //       react: 'React',
+    //       'jsx-runtime': 'jsxRuntime',
+    //       'react-native': 'ReactNative',
+    //       'react-dom': 'ReactDOM',
     //     },
     //   },
     // },
@@ -62,6 +76,21 @@ export default defineConfig({
     coverage: {
       reportsDirectory: '../../coverage/apps/core-frontend',
       provider: 'v8',
+    },
+  },
+
+  resolve: {
+    extensions: extensions,
+    alias: {
+      'react-native': 'react-native-web',
+    },
+  },
+
+  optimizeDeps: {
+    esbuildOptions: {
+      resolveExtensions: extensions,
+      jsx: 'automatic',
+      loader: { '.js': 'jsx' },
     },
   },
 });
