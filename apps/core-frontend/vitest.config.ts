ed1b08b1839c01baee5b914157634448224a9252
Irena Laemmerer <irena.lammerer@nrc.no>
2024-03-20 14:07:44 +0100

    [CORE24-114] task(frontend): test config split

---
+++ apps/core-frontend/vitest.config.ts
@@ -0,0 +1,19 @@
+/// <reference types='vitest' />
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test:{
+  globals: true,
+      cache: {
+        dir: '../../node_modules/.vitest',
+      },
+      environment: 'jsdom',
+      include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+
+      reporters: ['default'],
+      coverage: {
+        reportsDirectory: '../../coverage/apps/core-frontend',
+        provider: 'v8',
+      },
+    },
+});
