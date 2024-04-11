fa1c200c64d467ba980569392f572f2df3cd7ed3
David Figueroa <david.figueroa@nrc.no>
2024-04-11 10:00:13 +0200

    added auth excluded path for scim/v2

--- deploy/terraform/web_app/main.tf
+++ deploy/terraform/web_app/main.tf
@@ -130,6 +130,7 @@ resource "azurerm_linux_web_app" "app" {
     unauthenticated_action = "RedirectToLoginPage"
     default_provider = "custom_oidc_v2"
     forward_proxy_convention = "Standard"
+    excluded_paths = ["/scim/v2"]
 
     custom_oidc_v2 {
         name = "oidc"
