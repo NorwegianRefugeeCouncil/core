cf5993b0f7768fc596d3a1cfbcba44ea6a2ddce8
David Figueroa <david.figueroa@nrc.no>
2024-04-11 13:18:54 +0200

    fix: moved okta_id column addition to separate migration

--- libs/db/migrations/20240326111340_create-users-table.ts
+++ libs/db/migrations/20240326111340_create-users-table.ts
@@ -3,7 +3,6 @@ import type { Knex } from 'knex';
 export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTable('users', (table) => {
     table.uuid('id').primary();
-    table.string('okta_id').unique();
     table.string('user_name').unique().notNullable();
     table.string('first_name');
     table.string('last_name');
