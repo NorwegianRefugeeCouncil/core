5b16e514cb4479f9300f23b7b15e96f3bad59290
Ben McAlindin <bmcalindin@gmail.com>
2024-04-12 20:27:27 +0200

    Add /me endpoint

--- apps/core-api/src/controllers/api.controller.ts
+++ apps/core-api/src/controllers/api.controller.ts
@@ -6,4 +6,17 @@ const router = Router();
 router.use(json({ type: ['application/json'] }));
 router.use(userRouter);
 
+router.get('/me', async (req, res, next) => {
+  try {
+    const user = req.user;
+    if (user) {
+      res.status(200).json(user);
+    } else {
+      res.sendStatus(401);
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
 export { router as apiRouter };
