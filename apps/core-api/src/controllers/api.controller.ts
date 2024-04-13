53e8afd966357968f44e281b1e2735770dac2120
Ben McAlindin <bmcalindin@gmail.com>
2024-04-13 10:09:25 +0200

    Move me endpoint to a controller

--- apps/core-api/src/controllers/api.controller.ts
+++ apps/core-api/src/controllers/api.controller.ts
@@ -2,21 +2,12 @@ import { Router, json } from 'express';
 
 import { userRouter } from './user.controller';
 
+import { meRouter } from './me.controller';
+
 const router = Router();
 router.use(json({ type: ['application/json'] }));
 router.use(userRouter);
 
-router.get('/me', async (req, res, next) => {
-  try {
-    const user = req.user;
-    if (user) {
-      res.status(200).json(user);
-    } else {
-      res.sendStatus(401);
-    }
-  } catch (error) {
-    next(error);
-  }
-});
+router.use('/me', meRouter);
 
 export { router as apiRouter };
