c924a82af2fba65746a4dd429bc2b321c80fec4c
Ben McAlindin <bmcalindin@gmail.com>
2024-04-15 16:56:22 +0200

    Add a list users endpoint for debugging

--- apps/core-api/src/controllers/me.controller.ts
+++ apps/core-api/src/controllers/me.controller.ts
@@ -1,5 +1,7 @@
 import { Router } from 'express';
 
+import * as UserService from '../services/user.service';
+
 const router = Router();
 
 router.get('/', async (req, res, next) => {
@@ -10,6 +12,15 @@ router.get('/', async (req, res, next) => {
     } else {
       res.sendStatus(401);
     }
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get('/allusers', async (req, res, next) => {
+  try {
+    const users = await UserService.list(0, 100);
+    res.status(200).json({ users });
   } catch (error) {
     next(error);
   }
