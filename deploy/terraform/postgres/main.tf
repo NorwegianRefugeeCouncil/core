d40a70dcd89e8dd0c083eb7e3afc62ac1721575b
Ben McAlindin <bmcalindin@gmail.com>
2024-04-15 17:11:51 +0200

    Disable require_secure_transport for pg

--- deploy/terraform/postgres/main.tf
+++ deploy/terraform/postgres/main.tf
@@ -82,6 +82,13 @@ resource "azurerm_postgresql_flexible_server_database" "db" {
   collation = "en_US.utf8"
 }
 
+resource "azurerm_postgresql_flexible_server_configuration" "require_secure_transport" {
+  provider  = azurerm.runtime
+  name      = "require_secure_transport"
+  server_id = azurerm_postgresql_flexible_server.postgres.id
+  value     = "off"
+}
+
 resource "azurerm_monitor_metric_alert" "postgres_cpu_over_threshold" {
   provider            = azurerm.runtime
   name                = "postgres-cpu-over-threshold-${local.environment}"
