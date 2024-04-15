3558aee8f56c2ef68a0b2d62de2c61f857e6e86e
Ben McAlindin <bmcalindin@gmail.com>
2024-04-15 21:23:20 +0200

    Add list users endpoint

--- apps/core-api/src/controllers/user.controller.ts
+++ apps/core-api/src/controllers/user.controller.ts
@@ -1,4 +1,7 @@
 import { Router } from 'express';
+import { z } from 'zod';
+
+import * as UserService from '../services/user.service';
 
 const router = Router();
 
@@ -14,3 +17,21 @@ router.get('/me', async (req, res, next) => {
     next(error);
   }
 });
+
+// TODO: Move to models lib
+const paginationSchema = z.object({
+  startIndex: z.number().int().positive().default(0),
+  limit: z.number().int().positive().default(100),
+});
+
+router.get('/user', async (req, res, next) => {
+  try {
+    const { startIndex, limit } = paginationSchema.parse(req.query);
+    const users = await UserService.list(startIndex, limit);
+    res.status(200).json({ users });
+  } catch (error) {
+    next(error);
+  }
+});
+
+export { router as userRouter };
