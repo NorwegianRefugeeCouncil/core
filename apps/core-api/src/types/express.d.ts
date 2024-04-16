d0efdac1676adae0f32c95ac46e85c8fb6a3b22b
Ben McAlindin <bmcalindin@gmail.com>
2024-04-16 09:31:18 +0200

    Fix type

--- apps/core-api/src/types/express.d.ts
+++ apps/core-api/src/types/express.d.ts
@@ -1,11 +1,11 @@
-import { User } from '../models/user.model';
+import { User as CoreUser } from '../models/user.model';
 
 export {};
 
 declare global {
-  declare namespace Express {
-    export interface Request {
-      user?: User;
+  namespace Express {
+    interface User extends CoreUser {
+      __ignore__: undefined; // HACK: This is a workaround to prevent TS from removing the declaration
     }
   }
 }
