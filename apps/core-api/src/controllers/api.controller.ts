db03c2c4c2ab7365f1408d36d635bfcc2fe9e8c3
Ben McAlindin <bmcalindin@gmail.com>
2024-04-16 11:39:56 +0200

    Add api create endpoint for prm

--- apps/core-api/src/controllers/api.controller.ts
+++ apps/core-api/src/controllers/api.controller.ts
@@ -1,9 +1,11 @@
 import { Router, json } from 'express';
 
 import { userRouter } from './user.controller';
+import { prmRouter } from './prm.controller';
 
 const router = Router();
 router.use(json({ type: ['application/json'] }));
 router.use(userRouter);
+router.use(prmRouter);
 
 export { router as apiRouter };
