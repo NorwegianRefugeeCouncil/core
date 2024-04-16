085b3116683b80637f9f43da65994c7bfb8fd2e7
Ben McAlindin <bmcalindin@gmail.com>
2024-04-16 09:25:10 +0200

    Add todo

--- deploy/terraform/postgres/main.tf
+++ deploy/terraform/postgres/main.tf
@@ -82,6 +82,7 @@ resource "azurerm_postgresql_flexible_server_database" "db" {
   collation = "en_US.utf8"
 }
 
+// TODO: Review with DevOps team
 resource "azurerm_postgresql_flexible_server_configuration" "require_secure_transport" {
   provider  = azurerm.runtime
   name      = "require_secure_transport"
