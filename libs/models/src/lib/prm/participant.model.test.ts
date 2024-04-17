bb72ea3f740bc1bb0a4165453bc021d480da5f0a
Ben McAlindin <ben.mcalindin@nrc.no>
2024-04-17 18:35:41 +0200

    Task/core24 208 db migrations (#26)
    * Add enum types
    
    * Add db migrations
    
    * cleanup
    
    * Add participant schema (#27)
    
    * Allow for optional fields
    
    * Fix tests

--- libs/models/src/lib/prm/participant.model.test.ts
+++ libs/models/src/lib/prm/participant.model.test.ts
@@ -1,5 +1,144 @@
-describe('Participant model', () => {
-  it('should pass', () => {
-    expect(true).toBe(true);
+import { ulid } from 'ulidx';
+import { faker } from '@faker-js/faker';
+
+import {
+  ParticipantSchema,
+  ParticipantDefinitionSchema,
+} from './participant.model';
+
+describe('ParticipantSchema', () => {
+  it('should validate a valid participant object', () => {
+    const participant = {
+      id: ulid(),
+      firstName: 'John',
+      lastName: 'Doe',
+      dateOfBirth: new Date('1990-01-01'),
+      sex: 'male',
+      consentGdpr: true,
+      consentReferral: true,
+      contactDetails: [
+        {
+          id: faker.string.uuid(),
+          contactDetailType: 'email',
+          value: 'john.doe@example.com',
+        },
+        {
+          id: faker.string.uuid(),
+          contactDetailType: 'phone_number',
+          value: '1234567890',
+        },
+      ],
+      identification: [
+        {
+          id: faker.string.uuid(),
+          identificationType: 'unhcr_id',
+          identificationNumber: '1234567890',
+          isPrimary: true,
+        },
+      ],
+    };
+
+    const result = ParticipantSchema.parse(participant);
+
+    expect(result).toEqual(participant);
+  });
+
+  it('should not validate an invalid participant object', () => {
+    const participant = {
+      id: '1234567890abcdef',
+      firstName: 'John',
+      lastName: 'Doe',
+      dateOfBirth: new Date('1990-01-01'),
+      sex: 'male',
+      consentGdpr: true,
+      consentReferral: true,
+      contactDetails: [
+        {
+          contactDetailType: 'email',
+          value: 'john.doe@example.com',
+        },
+        {
+          contactDetailType: 'phone_number',
+          value: '1234567890',
+        },
+      ],
+      identification: [
+        {
+          identificationType: 'unhcr_id',
+          identificationNumber: '1234567890',
+          isPrimary: true,
+        },
+      ],
+    };
+
+    const result = ParticipantSchema.safeParse(participant);
+
+    expect(result.success).toBe(false);
+  });
+});
+
+describe('ParticipantDefinitionSchema', () => {
+  it('should validate a valid participant definition object', () => {
+    const participantDefinition = {
+      firstName: 'John',
+      lastName: 'Doe',
+      dateOfBirth: new Date('1990-01-01'),
+      sex: 'male',
+      consentGdpr: true,
+      consentReferral: true,
+      contactDetails: [
+        {
+          contactDetailType: 'email',
+          value: 'john.doe@example.com',
+        },
+        {
+          contactDetailType: 'phone_number',
+          value: '1234567890',
+        },
+      ],
+      identification: [
+        {
+          identificationType: 'unhcr_id',
+          identificationNumber: '1234567890',
+          isPrimary: true,
+        },
+      ],
+    };
+
+    const result = ParticipantDefinitionSchema.parse(participantDefinition);
+
+    expect(result).toEqual(participantDefinition);
+  });
+
+  it('should not validate an invalid participant definition object', () => {
+    const participantDefinition = {
+      firstName: 'John',
+      lastName: 'Doe',
+      dateOfBirth: new Date('1990-01-01'),
+      sex: 'invalid_sex',
+      consentGdpr: true,
+      consentReferral: true,
+      contactDetails: [
+        {
+          contactDetailType: 'email',
+          value: 'john.doe@example.com',
+        },
+        {
+          contactDetailType: 'phone_number',
+          value: '1234567890',
+        },
+      ],
+      identification: [
+        {
+          identificationType: 'unhcr_id',
+          identificationNumber: '1234567890',
+          isPrimary: true,
+        },
+      ],
+    };
+
+    const result = ParticipantDefinitionSchema.safeParse(participantDefinition);
+
+    expect(result.success).toBe(false);
   });
 });
--- libs/models/src/lib/prm/participant.model.ts
+++ libs/models/src/lib/prm/participant.model.ts
@@ -1,9 +1,157 @@
 import { z } from 'zod';
 
-export const ParticipantDefinitionSchema = z.object({});
+export enum Sex {
+  Male = 'male',
+  Female = 'female',
+  Other = 'other',
+  PreferNotToAnswer = 'prefer_not_to_answer',
+}
+export const SexSchema = z.nativeEnum(Sex);
+
+export enum ContactMeans {
+  Phone = 'phone',
+  Whatsapp = 'whatsapp',
+  Email = 'email',
+  Visit = 'visit',
+  Other = 'other',
+}
+export const ContactMeansSchema = z.nativeEnum(ContactMeans);
+
+export enum ContactDetailType {
+  Email = 'email',
+  PhoneNumber = 'phone_number',
+}
+export const ContactDetailTypeSchema = z.nativeEnum(ContactDetailType);
+
+export enum IdentificationType {
+  UnhcrId = 'unhcr_id',
+  Passport = 'passport',
+  NationalId = 'national_id',
+  Other = 'other',
+}
+export const IdentificationTypeSchema = z.nativeEnum(IdentificationType);
+
+export enum DisplacementStatus {
+  Idp = 'idp',
+  Refugee = 'refugee',
+  HostCommunity = 'host_community',
+  Returnee = 'returnee',
+  NonDisplaced = 'non_displaced',
+  AsylumSeeker = 'asylum_seeker',
+  Other = 'other',
+}
+export const DisplacementStatusSchema = z.nativeEnum(DisplacementStatus);
+
+export enum EngagementContext {
+  HouseVisit = 'house_visit',
+  FieldActivity = 'field_activity',
+  Referred = 'referred',
+  InOfficeMeeting = 'in_office_meeting',
+  RemoteChannels = 'remote_channels',
+}
+export const EngagementContextSchema = z.nativeEnum(EngagementContext);
+
+export enum DisabilityLevel {
+  One = '1',
+  Two = '2',
+  Three = '3',
+  Four = '4',
+}
+export const DisabilityLevelSchema = z.nativeEnum(DisabilityLevel);
+
+export enum YesNoUnknown {
+  Yes = 'yes',
+  No = 'no',
+  Unknown = 'unknown',
+}
+export const YesNoUnknownSchema = z.nativeEnum(YesNoUnknown);
+
+const ParticipantDetailsSchema = z.object({
+  firstName: z.string().max(100).optional(),
+  middleName: z.string().max(100).optional(),
+  lastName: z.string().max(100).optional(),
+  nativeName: z.string().max(100).optional(),
+  motherName: z.string().max(100).optional(),
+  preferredName: z.string().max(100).optional(),
+  dateOfBirth: z.date().optional(),
+  nrcId: z.string().max(40).optional(),
+  residence: z.string().optional(),
+  contactMeansComment: z.string().optional(),
+  consentGdpr: z.boolean(),
+  consentReferral: z.boolean(),
+  sex: SexSchema.optional(),
+  preferredContactMeans: ContactMeansSchema.optional(),
+  displacementStatus: DisplacementStatusSchema.optional(),
+  engagementContext: EngagementContextSchema.optional(),
+});
+
+const ParticipantDisabilitySchema = z.object({
+  hasDisabilityPwd: z.boolean(),
+  disabilityPwdComment: z.string(),
+  hasDisabilityVision: z.boolean(),
+  disabilityVisionLevel: DisabilityLevelSchema,
+  hasDisabilityHearing: z.boolean(),
+  disabilityHearingLevel: DisabilityLevelSchema,
+  hasDisabilityMobility: z.boolean(),
+  disabilityMobilityLevel: DisabilityLevelSchema,
+  hasDisabilityCognition: z.boolean(),
+  disabilityCognitionLevel: DisabilityLevelSchema,
+  hasDisabilitySelfcare: z.boolean(),
+  disabilitySelfcareLevel: DisabilityLevelSchema,
+  hasDisabilityCommunication: z.boolean(),
+  disabilityCommunicationLevel: DisabilityLevelSchema,
+  isChildAtRisk: YesNoUnknownSchema,
+  isElderAtRisk: YesNoUnknownSchema,
+  isWomanAtRisk: YesNoUnknownSchema,
+  isSingleParent: YesNoUnknownSchema,
+  isSeparatedChild: YesNoUnknownSchema,
+  isPregnant: YesNoUnknownSchema,
+  isLactating: YesNoUnknownSchema,
+  hasMedicalCondition: YesNoUnknownSchema,
+  needsLegalPhysicalProtection: YesNoUnknownSchema,
+  vulnerabilityComments: z.string(),
+});
+
+const ContactDetailsSchema = z.object({
+  contactDetailType: ContactDetailTypeSchema,
+  value: z.string(),
+});
+
+const IdentificationSchema = z.object({
+  identificationType: IdentificationTypeSchema,
+  identificationNumber: z.string(),
+  isPrimary: z.boolean(),
+});
+
+export const ParticipantDefinitionSchema = ParticipantDetailsSchema.merge(
+  z.object({
+    disabilities: ParticipantDisabilitySchema.optional(),
+    contactDetails: z.array(ContactDetailsSchema),
+    identification: z.array(IdentificationSchema),
+  }),
+);
 
 export type ParticipantDefinition = z.infer<typeof ParticipantDefinitionSchema>;
 
-export const ParticipantSchema = z.object({});
+export const ParticipantSchema = ParticipantDefinitionSchema.merge(
+  z.object({
+    id: z.string().ulid(),
+    disabilities: ParticipantDisabilitySchema.optional(),
+    contactDetails: z.array(
+      ContactDetailsSchema.merge(
+        z.object({
+          id: z.string().uuid(),
+        }),
+      ),
+    ),
+    identification: z.array(
+      IdentificationSchema.merge(
+        z.object({
+          id: z.string().uuid(),
+        }),
+      ),
+    ),
+  }),
+);
 
 export type Participant = z.infer<typeof ParticipantSchema>;
