363f8236b265ff06314de6d0759aaa4e8c8f6aa4
Ben McAlindin <bmcalindin@gmail.com>
2024-04-17 15:57:09 +0200

    Fix file name

---
+++ libs/prm-engine/src/lib/stores/index.ts
@@ -0,0 +1,8 @@
+import { EntityType } from '@nrcno/core-models';
+
+import { ParticipantStore } from './participant.store';
+import { BaseStore } from './base.store';
+
+export const PrmStore = {
+  participant: ParticipantStore,
+};
