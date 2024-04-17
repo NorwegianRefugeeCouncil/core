ca0c6eebee624bad917254a093a5f90d5fc145e5
Ben McAlindin <bmcalindin@gmail.com>
2024-04-17 10:37:56 +0200

    Add enum types

--- libs/models/src/lib/prm/participant.model.ts
+++ libs/models/src/lib/prm/participant.model.ts
@@ -1,5 +1,71 @@
 import { z } from 'zod';
 
+export enum Sex {
+  Male = 'male',
+  Female = 'female',
+  Other = 'other',
+  PreferNotToAnswer = 'prefer_not_to_answer',
+}
+export const SexSchema = z.nativeEnum(Sex);
+
+export enum ContactMeans {
+  Phone = 'phone',
+  Whatsapp = 'whatsapp',
+  Email = 'email',
+  Visit = 'visit',
+  Other = 'other',
+}
+export const ContactMeansSchema = z.nativeEnum(ContactMeans);
+
+export enum ContactDetailType {
+  Email = 'email',
+  PhoneNumber = 'phone_number',
+}
+export const ContactDetailTypeSchema = z.nativeEnum(ContactDetailType);
+
+export enum IdentificationType {
+  UnhcrId = 'unhcr_id',
+  Passport = 'passport',
+  NationalId = 'national_id',
+  Other = 'other',
+}
+export const IdentificationTypeSchema = z.nativeEnum(IdentificationType);
+
+export enum DisplacementStatus {
+  Idp = 'idp',
+  Refugee = 'refugee',
+  HostCommunity = 'host_community',
+  Returnee = 'returnee',
+  NonDisplaced = 'non_displaced',
+  AsylumSeeker = 'asylum_seeker',
+  Other = 'other',
+}
+export const DisplacementStatusSchema = z.nativeEnum(DisplacementStatus);
+
+export enum EngagementContext {
+  HouseVisit = 'house_visit',
+  FieldActivity = 'field_activity',
+  Referred = 'referred',
+  InOfficeMeeting = 'in_office_meeting',
+  RemoteChannels = 'remote_channels',
+}
+export const EngagementContextSchema = z.nativeEnum(EngagementContext);
+
+export enum DisabilityLevel {
+  One = '1',
+  Two = '2',
+  Three = '3',
+  Four = '4',
+}
+export const DisabilityLevelSchema = z.nativeEnum(DisabilityLevel);
+
+export enum YesNoUnknown {
+  Yes = 'yes',
+  No = 'no',
+  Unknown = 'unknown',
+}
+export const YesNoUnknownSchema = z.nativeEnum(YesNoUnknown);
+
 export const ParticipantDefinitionSchema = z.object({});
 
 export type ParticipantDefinition = z.infer<typeof ParticipantDefinitionSchema>;
