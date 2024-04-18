68200b342d0b62dceac8097d8e23f498bc3d40f7
Ben McAlindin <bmcalindin@gmail.com>
2024-04-18 09:32:53 +0200

    Use enum when exporting

--- libs/prm-engine/src/lib/stores/index.ts
+++ libs/prm-engine/src/lib/stores/index.ts
@@ -1,8 +1,7 @@
 import { EntityType } from '@nrcno/core-models';
 
 import { ParticipantStore } from './participant.store';
-import { BaseStore } from './base.store';
 
 export const PrmStore = {
-  participant: ParticipantStore,
+  [EntityType.Participant]: ParticipantStore,
 };
