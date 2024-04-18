fe6f5f1b31a8055ac8ea9fb273bc4693923f3db5
Ben McAlindin <bmcalindin@gmail.com>
2024-04-18 11:25:14 +0200

    Add endpoint for entity create flow

--- apps/core-api/src/controllers/prm.controller.ts
+++ apps/core-api/src/controllers/prm.controller.ts
@@ -1,12 +1,24 @@
 import { Router } from 'express';
 
 import { PrmService } from '@nrcno/core-prm-engine';
+import { EntityTypeSchema } from '@nrcno/core-models';
 
 const router = Router();
 
 router.post('/prm/:entityType', async (req, res, next) => {
   try {
-    throw new Error('Not implemented');
+    const entityType = EntityTypeSchema.parse(req.params.entityType);
+    const prmService = PrmService[entityType];
+
+    if (!prmService) {
+      res.sendStatus(404);
+      return;
+    }
+
+    const entityDefinition = req.body; // TODO: Validate entity definition
+    const createdEntity = await prmService.create(entityDefinition);
+
+    res.status(201).json(createdEntity);
   } catch (error) {
     next(error);
   }
