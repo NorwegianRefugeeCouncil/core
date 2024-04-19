ec4e4bd19fe240c0e689a355e735fd413b22b655
Ben McAlindin <bmcalindin@gmail.com>
2024-04-19 18:13:18 +0200

    Pretty logs for development

--- libs/logger/src/lib/logger.ts
+++ libs/logger/src/lib/logger.ts
@@ -1,10 +1,20 @@
 import { pino } from 'pino';
+import pretty from 'pino-pretty';
 
 let logger: pino.Logger;
 
 export const getLogger = (): pino.Logger => {
   if (!logger) {
-    logger = pino();
+    if (process.env.NODE_ENV === 'production') {
+      logger = pino();
+    } else {
+      logger = pino(
+        { level: 'debug' },
+        pretty({
+          colorize: true,
+        }),
+      );
+    }
   }
   return logger;
 };
