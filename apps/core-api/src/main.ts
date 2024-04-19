a3b6a6ec542811ea8c28e90f72bff38644fff170
Ben McAlindin <bmcalindin@gmail.com>
2024-04-19 15:39:08 +0200

    Remove nocache for api

--- apps/core-api/src/main.ts
+++ apps/core-api/src/main.ts
@@ -45,7 +45,7 @@ app.use(oidc());
 
 app.use('/healthz', nocache(), healthzRouter);
 app.use('/scim/v2', nocache(), scimRouter);
-app.use('/api', [nocache(), requireAuthentication], apiRouter);
+app.use('/api', requireAuthentication, apiRouter);
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'static', 'index.html'));
