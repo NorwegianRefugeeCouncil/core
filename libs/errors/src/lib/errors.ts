51b8e3a3ffa05aaf1183e6f54583f14c1fb2288a
David Figueroa <david.figueroa@nrc.no>
2024-04-22 17:14:45 +0200

    fix: little cleanup and fixes

--- libs/errors/src/lib/errors.ts
+++ libs/errors/src/lib/errors.ts
@@ -5,11 +5,3 @@ export class AlreadyExistsError extends Error {
     Object.setPrototypeOf(this, new.target.prototype); // maintain proper stack trace
   }
 }
-
-export class UnauthorisedErrorUnauthorisedError extends Error {
-  constructor(message?: string) {
-    super(message);
-    this.name = 'UnauthorisedError';
-    Object.setPrototypeOf(this, new.target.prototype); // maintain proper stack trace
-  }
-}
