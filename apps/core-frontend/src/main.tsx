6b7b8f5a7c0965dcca5637fb23947322fd25bdea
Ben McAlindin <bmcalindin@gmail.com>
2024-04-25 08:45:44 +0200

    Linting

--- apps/core-frontend/src/main.tsx
+++ apps/core-frontend/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
+
 import { theme } from '@nrcno/core-theme';
 
 import { router } from './routes';
