a35c03df8b80249d796a9ba67dee60589cbca1b3
Ben McAlindin <bmcalindin@gmail.com>
2024-04-26 09:03:43 +0200

    Fix tests

--- libs/prm-components/src/lib/hooks/useEntityDetailPage.hook.test.ts
+++ libs/prm-components/src/lib/hooks/useEntityDetailPage.hook.test.ts
@@ -1,5 +1,4 @@
 import { renderHook } from '@testing-library/react-hooks';
-import { useLoaderData } from 'react-router-dom';
 import { vi, Mock } from 'vitest';
 import { EntityType } from '@nrcno/core-models';
 
@@ -9,15 +8,13 @@ import { config } from '../config';
 import { SubmitStatus } from './useApiReducer.hook';
 import { useEntityDetailPage } from './useEntityDetailPage.hook';
 
-vi.mock('react-router-dom', () => ({
-  useLoaderData: vi.fn(),
-}));
-
 vi.mock('../prm.context', () => ({
   usePrmContext: vi.fn(),
 }));
 
 const prmContextData = {
+  entityType: EntityType.Participant,
+  entityId: undefined,
   create: {
     onCreateEntity: vi.fn(),
     status: SubmitStatus.IDLE,
@@ -27,32 +24,24 @@ const prmContextData = {
 };
 
 describe('useEntityDetailPage', () => {
-  const loaderData = {
-    mode: 'create',
-    entityType: EntityType.Participant,
-  };
-
   beforeEach(() => {
     vi.clearAllMocks();
   });
 
   describe('create', () => {
     it('should call useLoaderData with the correct arguments', () => {
-      (useLoaderData as Mock).mockReturnValue(loaderData);
       (usePrmContext as Mock).mockReturnValue(prmContextData);
 
-      renderHook(() => useEntityDetailPage());
-
-      expect(useLoaderData).toHaveBeenCalledWith();
+      renderHook(() => useEntityDetailPage('create'));
     });
 
     it('should return the correct values', () => {
-      (useLoaderData as Mock).mockReturnValue(loaderData);
       (usePrmContext as Mock).mockReturnValue(prmContextData);
 
-      const { result } = renderHook(() => useEntityDetailPage());
+      const { result } = renderHook(() => useEntityDetailPage('create'));
 
       expect(result.current).toEqual({
+        mode: 'create',
         onSubmit: expect.any(Function),
         entityType: EntityType.Participant,
         config: config[EntityType.Participant].detail,
@@ -64,52 +53,51 @@ describe('useEntityDetailPage', () => {
     });
 
     it('should set isLoading to true when status is SUBMITTING', () => {
-      (useLoaderData as Mock).mockReturnValue(loaderData);
       (usePrmContext as Mock).mockReturnValue({
+        ...prmContextData,
         create: {
           ...prmContextData.create,
           status: SubmitStatus.SUBMITTING,
         },
       });
 
-      const { result } = renderHook(() => useEntityDetailPage());
+      const { result } = renderHook(() => useEntityDetailPage('create'));
 
       expect(result.current.isLoading).toBe(true);
     });
 
     it('should set isError to true when status is ERROR', () => {
-      (useLoaderData as Mock).mockReturnValue(loaderData);
       (usePrmContext as Mock).mockReturnValue({
+        ...prmContextData,
         create: {
           ...prmContextData.create,
           status: SubmitStatus.ERROR,
         },
       });
 
-      const { result } = renderHook(() => useEntityDetailPage());
+      const { result } = renderHook(() => useEntityDetailPage('create'));
 
       expect(result.current.isError).toBe(true);
     });
 
     it('should set isSuccess to true when status is SUCCESS', () => {
-      (useLoaderData as Mock).mockReturnValue(loaderData);
       (usePrmContext as Mock).mockReturnValue({
+        ...prmContextData,
         create: {
           ...prmContextData.create,
           status: SubmitStatus.SUCCESS,
         },
       });
 
-      const { result } = renderHook(() => useEntityDetailPage());
+      const { result } = renderHook(() => useEntityDetailPage('create'));
 
       expect(result.current.isSuccess).toBe(true);
     });
 
     it('should parse the form data and call onCreateEntity when handleSubmit is called', () => {
-      (useLoaderData as Mock).mockReturnValue(loaderData);
       (usePrmContext as Mock).mockReturnValue(prmContextData);
 
-      const { result } = renderHook(() => useEntityDetailPage());
+      const { result } = renderHook(() => useEntityDetailPage('create'));
 
       const form = document.createElement('form');
       const createInputElement = (name: string, value: string) => {
@@ -126,6 +114,9 @@ describe('useEntityDetailPage', () => {
       form.appendChild(
         createInputElement('contactDetails.0.contactDetailType', 'Email'),
       );
+
+      if (!result.current.onSubmit)
+        throw new Error('onSubmit is not defined on the hook');
 
       result.current.onSubmit(form);
 
