82b00d29ddd84722667a60f084a6523abfb57d04
Ben McAlindin <bmcalindin@gmail.com>
2024-04-28 13:33:35 +0200

    use router

--- apps/core-api/src/controllers/api.controller.ts
+++ apps/core-api/src/controllers/api.controller.ts
@@ -2,10 +2,12 @@ import { Router, json } from 'express';
 
 import { userRouter } from './user.controller';
 import { prmRouter } from './prm.controller';
+import { deduplicationRouter } from './deduplication.controller';
 
 const router = Router();
 router.use(json({ type: ['application/json'] }));
 router.use(userRouter);
 router.use(prmRouter);
+router.use(deduplicationRouter);
 
 export { router as apiRouter };
