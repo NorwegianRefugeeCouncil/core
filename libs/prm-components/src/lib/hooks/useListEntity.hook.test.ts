a8052ec2007c619a86137bc003ad1ed7646a0a57
Ben McAlindin <bmcalindin@gmail.com>
2024-05-08 17:18:21 +0200

    Rename

--- libs/prm-components/src/lib/hooks/useListEntity.hook.test.ts
+++ libs/prm-components/src/lib/hooks/useListEntity.hook.test.ts
@@ -1,17 +1,13 @@
 import { vi } from 'vitest';
 import { act, renderHook } from '@testing-library/react-hooks';
-import {
-  EntityListItem,
-  PaginatedResponse,
-  Pagination,
-} from '@nrcno/core-models';
+import { Pagination } from '@nrcno/core-models';
 
 import { useListEntity } from './useListEntity.hook';
 import { SubmitStatus } from './useApiReducer.hook';
 
 const pagination: Pagination = {
   startIndex: 0,
-  limit: 100,
+  pageSize: 100,
 };
 
 describe('useListEntity', () => {
