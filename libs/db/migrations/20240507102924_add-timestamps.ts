f4b65e0701f56d78a329b11a13182f5ea7b4da62
David Figueroa <dfigueroa87@users.noreply.github.com>
2024-05-08 16:23:46 +0200

    task(db): Added created and updated timestamps to participant tables (#64)

---
+++ libs/db/migrations/20240507102924_add-timestamps.ts
@@ -0,0 +1,47 @@
+import type { Knex } from 'knex';
+
+const tables = [
+  'participants',
+  'participant_disabilities',
+  'participant_identifications',
+  'participant_contact_details',
+  'participant_nationalities',
+  'participant_languages',
+];
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.raw(`
+    CREATE OR REPLACE FUNCTION update_updated_at_column()
+    RETURNS TRIGGER AS $$
+    BEGIN
+       NEW.updated_at = current_timestamp(3);
+       RETURN NEW;
+    END;
+    $$ language 'plpgsql';
+  `);
+
+  for (const table of tables) {
+    await knex.schema.table(table, (table) => {
+      table.timestamp('created_at', { precision: 3 }).defaultTo(knex.fn.now(3));
+      table.timestamp('updated_at', { precision: 3 }).defaultTo(knex.fn.now(3));
+    });
+
+    await knex.raw(`
+      CREATE TRIGGER update_timestamp
+      BEFORE UPDATE ON ${table}
+      FOR EACH ROW
+      EXECUTE PROCEDURE update_updated_at_column();
+    `);
+  }
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.raw(`DROP FUNCTION update_updated_at_column CASCADE;`);
+
+  for (const table of tables) {
+    await knex.schema.table(table, (table) => {
+      table.dropColumn('created_at');
+      table.dropColumn('updated_at');
+    });
+  }
+}
