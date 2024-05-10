23d1a666490f7a1ae4ac474e80a04e277e9c1994
Ben McAlindin <ben.mcalindin@nrc.no>
2024-05-10 10:18:21 +0200

    Auth screen (#65)
    * Use /me endpoint for authentication check
    
    * Change page header to be h1
    
    * Don't run frontend e2e

--- apps/core-frontend/src/contexts/user.context.tsx
+++ apps/core-frontend/src/contexts/user.context.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { AxiosInstance } from 'axios';
 
 import { UserClient } from '@nrcno/core-clients';
+import { SubmitStatus } from '@nrcno/core-prm-components';
 
 import { MeState, useMe, defaultMeState } from '../hooks/useMe.hook';
 
@@ -22,6 +23,13 @@ export const UserProvider: React.FC<Props> = ({ axiosInstance, children }) => {
   const userClient = new UserClient(axiosInstance);
 
   const me = useMe(userClient);
+
+  React.useEffect(() => {
+    me.getMe();
+  }, []);
+
+  // Use /api/me as an authentication check
+  if (me.status !== SubmitStatus.SUCCESS) return;
 
   return <UserContext.Provider value={{ me }}>{children}</UserContext.Provider>;
 };
