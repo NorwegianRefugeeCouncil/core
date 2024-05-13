4470ab2d61ac436313c38d109655b48f9de0100c
Ben McAlindin <bmcalindin@gmail.com>
2024-05-13 10:41:24 +0200

    Improve scrolling to ensure pagination is visible

--- libs/prm-components/src/lib/pages/EntityListPage/EntityListPage.component.tsx
+++ libs/prm-components/src/lib/pages/EntityListPage/EntityListPage.component.tsx
@@ -1,10 +1,9 @@
-import { Heading, Spinner, Box } from '@chakra-ui/react';
+import { Heading, Spinner, Flex } from '@chakra-ui/react';
 import {
   DisplacementStatus,
   IdentificationType,
   Sex,
 } from '@nrcno/core-models';
-import { theme } from '@nrcno/core-theme';
 import { useEffect } from 'react';
 
 import { EntityList } from '../../components';
@@ -33,53 +32,47 @@ export const EntityListPage: React.FC = () => {
   }, [JSON.stringify(data)]);
 
   return (
-    <Box>
-      <Heading
-        pb="8"
-        bg={theme.colors.white}
-        top="0"
-        position="sticky"
-        zIndex={'docked'}
-      >
-        {entityType}
-      </Heading>
+    <Flex height="100%" direction="column">
+      <Heading pb="8">{entityType}</Heading>
       {isLoading && <Spinner colorScheme="primary" size="xl" />}
       {isError && <div>{error?.message}</div>}
       {isSuccess && <div>Success</div>}
-      <EntityList
-        config={config}
-        entityList={new Array(100).fill({
-          dateOfBirth: new Date('2006-10-12'),
-          displacementStatus: DisplacementStatus.AsylumSeeker,
-          firstName: 'first name',
-          id: 'id1',
-          lastName: 'lastname',
-          nationalities: ['en'],
-          identification: [
-            {
-              id: 'id1',
-              identificationType: IdentificationType.Passport,
-              identificationNumber: '123456789',
-              isPrimary: true,
+      <Flex flex={1} overflow="hidden">
+        <EntityList
+          config={config}
+          entityList={new Array(100).fill({
+            dateOfBirth: new Date('2006-10-12'),
+            displacementStatus: DisplacementStatus.AsylumSeeker,
+            firstName: 'first name',
+            id: 'id1',
+            lastName: 'lastname',
+            nationalities: ['en'],
+            identification: [
+              {
+                id: 'id1',
+                identificationType: IdentificationType.Passport,
+                identificationNumber: '123456789',
+                isPrimary: true,
+              },
+            ],
+            contactDetails: {
+              phones: [
+                {
+                  id: 'phone1',
+                  value: '12345678',
+                },
+              ],
+              emails: [
+                {
+                  id: 'email1',
+                  value: 'email@email.com',
+                },
+              ],
             },
-          ],
-          contactDetails: {
-            phones: [
-              {
-                id: 'phone1',
-                value: '12345678',
-              },
-            ],
-            emails: [
-              {
-                id: 'email1',
-                value: 'email@email.com',
-              },
-            ],
-          },
-          sex: Sex.Female,
-        })}
-      />
+            sex: Sex.Female,
+          })}
+        />
+      </Flex>
       <Flex justifyContent="flex-end">
         <Pagination
           pagination={pagination}
@@ -92,6 +85,6 @@ export const EntityListPage: React.FC = () => {
           totalPages={totalPages}
         />
       </Flex>
-      </Flex>
+    </Flex>
   );
 };
