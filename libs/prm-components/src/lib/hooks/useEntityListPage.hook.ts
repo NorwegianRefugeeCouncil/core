44bbc319a03437dec0ceccc635a83b6de775477a
Ben McAlindin <bmcalindin@gmail.com>
2024-05-13 10:31:06 +0200

    Load entites with pagination

--- libs/prm-components/src/lib/hooks/useEntityListPage.hook.ts
+++ libs/prm-components/src/lib/hooks/useEntityListPage.hook.ts
@@ -1,10 +1,17 @@
+import { useEffect } from 'react';
+import { Pagination } from '@nrcno/core-models';
+
 import { usePrmContext } from '../prm.context';
 import { config } from '../config';
 
 import { SubmitStatus } from './useApiReducer.hook';
 
-export const useEntityListPage = () => {
+export const useEntityListPage = (pagination: Pagination) => {
   const { entityType, list } = usePrmContext();
+
+  useEffect(() => {
+    list.listEntities(pagination);
+  }, [JSON.stringify(pagination)]);
 
   if (!entityType) {
     throw new Error('Entity type is required');
