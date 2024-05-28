a4e73c59e74296fb3d3b3822dc9b5b63ad0c6e26
Ben McAlindin <bmcalindin@gmail.com>
2024-05-28 14:52:47 +0200

    Add list and count to language store

---
+++ libs/prm-engine/src/lib/stores/language.store.integration.test.ts
@@ -0,0 +1,36 @@
+import { getDb } from '@nrcno/core-db';
+
+import { LanguageStore } from './language.store';
+
+describe('Language store', () => {
+  beforeAll(async () => {
+    getDb(undefined, (global as any).db);
+  });
+
+  describe('list', () => {
+    test('should return a list of participants', async () => {
+      const result = await LanguageStore.list({
+        startIndex: 0,
+        pageSize: 50,
+      });
+
+      expect(result).toHaveLength(50);
+      expect(result[0]).toEqual({
+        id: 'aaa',
+        enabled: true,
+      });
+      expect(result[1]).toEqual({
+        id: 'aab',
+        enabled: true,
+      });
+    });
+  });
+
+  describe('count', () => {
+    test('should return the count of participants', async () => {
+      const result = await LanguageStore.count();
+
+      expect(result).toBe(7910);
+    });
+  });
+});
---
+++ libs/prm-engine/src/lib/stores/language.store.ts
@@ -0,0 +1,35 @@
+import { z } from 'zod';
+
+import { Language, LanguageSchema, Pagination } from '@nrcno/core-models';
+import { getDb } from '@nrcno/core-db';
+
+import { ListStore } from './base.store';
+
+export type ILanguageStore = ListStore<Language>;
+
+const list: ILanguageStore['list'] = async (
+  pagination: Pagination,
+): Promise<Language[]> => {
+  const db = getDb();
+
+  const languages = await db('languages')
+    .select('*')
+    .limit(pagination.pageSize)
+    .offset(pagination.startIndex)
+    .orderBy('id');
+
+  return z.array(LanguageSchema).parse(languages);
+};
+
+const count: ILanguageStore['count'] = async (): Promise<number> => {
+  const db = getDb();
+
+  const [{ count }] = await db('languages').count();
+
+  return typeof count === 'string' ? parseInt(count, 10) : count;
+};
+
+export const LanguageStore: ILanguageStore = {
+  list,
+  count,
+};
