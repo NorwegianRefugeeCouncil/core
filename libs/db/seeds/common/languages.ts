6c336840e87fe1b814af46a161bd18324552b1b9
Ben McAlindin <bmcalindin@gmail.com>
2024-05-28 14:51:36 +0200

    Add language seeding from json file

--- libs/db/seeds/common/languages.ts
+++ libs/db/seeds/common/languages.ts
@@ -1,22 +1,28 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
 import { Knex } from 'knex';
 
 export async function seed(knex: Knex): Promise<void> {
-  // TODO: Import languages from a JSON file
-  const languages = [
-    { isoCode: 'en' },
-    { isoCode: 'es' },
-    { isoCode: 'fr' },
-    { isoCode: 'ar' },
-  ];
+  const languages: {
+    id: string;
+  }[] = JSON.parse(
+    fs.readFileSync(
+      path.resolve(__dirname, '../../constants/', 'languages.json'),
+      'utf8',
+    ),
+  ).map((lang: any) => ({
+    id: lang.id,
+  }));
 
   // Upsert all rows in languages
-  await knex('languages').insert(languages).onConflict('iso_code').merge();
+  await knex('languages').insert(languages).onConflict('id').merge();
 
   // Disable any rows not in languages
   await knex('languages')
     .whereNotIn(
-      'iso_code',
-      languages.map((lang) => lang.isoCode),
+      'id',
+      languages.map((lang) => lang.id),
     )
     .update({ enabled: false });
 }
