0fbed4292f04b0dd0a883b72416bb5a3bb0e8add
Ben McAlindin <bmcalindin@gmail.com>
2024-05-28 14:52:28 +0200

    Change schema lookups to handle partial keys

--- libs/models/src/lib/prm/entity.model.ts
+++ libs/models/src/lib/prm/entity.model.ts
@@ -14,54 +14,78 @@ import {
   ParticipantPartialUpdate,
   ParticipantUpdate,
 } from './participant.model';
+import { Language, LanguageSchema } from './language.model';
 
 export enum EntityType {
   Participant = 'participants',
+  Language = 'languages',
 }
 
 export const EntityTypeSchema = z.nativeEnum(EntityType);
 
 export const EntityIdSchema = z.string().ulid();
 
-export type Entity = Participant;
+export type Entity = Participant | Language;
 export type EntityDefinition = ParticipantDefinition;
-export type EntityListItem = ParticipantListItem;
+export type EntityListItem = ParticipantListItem | Language;
 export type EntityUpdate = ParticipantUpdate;
 export type EntityPartialUpdate = ParticipantPartialUpdate;
 export type EntityFiltering = ParticipantFiltering;
 
-const entitySchemaMap = {
-  [EntityType.Participant]: ParticipantSchema,
+export const getEntitySchema = (entityType: EntityType) => {
+  switch (entityType) {
+    case EntityType.Participant:
+      return ParticipantSchema;
+    case EntityType.Language:
+      return LanguageSchema;
+    default:
+      throw new Error(`No schema found for ${entityType}`);
+  }
 };
-export const getEntitySchema = (entityType: EntityType) =>
-  entitySchemaMap[entityType];
 
-const entityDefinitionSchemaMap = {
-  [EntityType.Participant]: ParticipantDefinitionSchema,
+export const getEntityDefinitionSchema = (entityType: EntityType) => {
+  switch (entityType) {
+    case EntityType.Participant:
+      return ParticipantDefinitionSchema;
+    default:
+      throw new Error(`No definition schema found for ${entityType}`);
+  }
 };
-export const getEntityDefinitionSchema = (entityType: EntityType) =>
-  entityDefinitionSchemaMap[entityType];
 
-const entityUpdateSchemaMap = {
-  [EntityType.Participant]: ParticipantUpdateSchema,
+export const getEntityUpdateSchema = (entityType: EntityType) => {
+  switch (entityType) {
+    case EntityType.Participant:
+      return ParticipantUpdateSchema;
+    default:
+      throw new Error(`No update schema found for ${entityType}`);
+  }
 };
-export const getEntityUpdateSchema = (entityType: EntityType) =>
-  entityUpdateSchemaMap[entityType];
 
-const entityListItemSchemaMap = {
-  [EntityType.Participant]: ParticipantListItemSchema,
+export const getEntityListItemSchema = (entityType: EntityType) => {
+  switch (entityType) {
+    case EntityType.Participant:
+      return ParticipantListItemSchema;
+    case EntityType.Language:
+      return LanguageSchema;
+    default:
+      throw new Error(`No list item schema found for ${entityType}`);
+  }
 };
-export const getEntityListItemSchema = (entityType: EntityType) =>
-  entityListItemSchemaMap[entityType];
 
-const entityListSortingFieldsMap = {
-  [EntityType.Participant]: ParticipantListSortingFields,
+export const getEntityListSortingFields = (entityType: EntityType) => {
+  switch (entityType) {
+    case EntityType.Participant:
+      return ParticipantListSortingFields;
+    default:
+      throw new Error(`No sorting fields found for ${entityType}`);
+  }
 };
-export const getEntityListSortingFields = (entityType: EntityType) =>
-  entityListSortingFieldsMap[entityType];
 
-const entityFilteringSchemaMap = {
-  [EntityType.Participant]: ParticipantFilteringSchema,
+export const getEntityFilteringSchema = (entityType: EntityType) => {
+  switch (entityType) {
+    case EntityType.Participant:
+      return ParticipantFilteringSchema;
+    default:
+      throw new Error(`No filtering schema found for ${entityType}`);
+  }
 };
-export const getEntityFilteringSchema = (entityType: EntityType) =>
-  entityFilteringSchemaMap[entityType];
