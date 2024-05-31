e896271388ade7eed202882e487283e7231c86af
Ben McAlindin <bmcalindin@gmail.com>
2024-05-31 09:24:49 +0200

    Add placeholder config

--- libs/prm-components/src/lib/config/index.ts
+++ libs/prm-components/src/lib/config/index.ts
@@ -5,15 +5,20 @@ import { participantConfig } from './participant';
 
 export * from './config.types';
 
+// TODO: We should support entity types with no/partial configuration
+// If we go to a page that the entity doesn't support it should give a 404
+const emptyConfig = {
+  detail: {
+    sections: [],
+  },
+  list: {
+    fields: [],
+  },
+  search: [],
+};
+
 export const configLoader: PrmUIConfigLoader = (staticData) => ({
   [EntityType.Participant]: participantConfig(staticData),
-  [EntityType.Language]: {
-    detail: {
-      sections: [],
-    },
-    list: {
-      fields: [],
-    },
-    search: [],
-  },
+  [EntityType.Language]: emptyConfig,
+  [EntityType.Nationality]: emptyConfig,
 });
