20735cfc930816d7f02076de57460aab1203cfe6
Ben McAlindin <bmcalindin@gmail.com>
2024-05-31 13:14:23 +0200

    Add filtering and sorting

--- libs/prm-engine/src/lib/stores/language.store.ts
+++ libs/prm-engine/src/lib/stores/language.store.ts
@@ -1,10 +1,14 @@
 import { z } from 'zod';
+import { Knex } from 'knex';
 
 import {
+  EntityType,
   Language,
   LanguageFilter,
   LanguageSchema,
   Pagination,
+  Sorting,
+  createSortingSchema,
 } from '@nrcno/core-models';
 import { getDb } from '@nrcno/core-db';
 
@@ -12,24 +16,37 @@ import { ListStore } from './base.store';
 
 export type ILanguageStore = ListStore<Language, LanguageFilter>;
 
+const filter = (filtering: LanguageFilter) => (builder: Knex.QueryBuilder) => {
+  if (filtering.enabled !== undefined) {
+    builder.where('enabled', filtering.enabled);
+  }
+};
+
 const list: ILanguageStore['list'] = async (
   pagination: Pagination,
+  { sort, direction }: Sorting = createSortingSchema(
+    EntityType.Participant,
+  ).parse({}),
+  filtering: LanguageFilter = {},
 ): Promise<Language[]> => {
   const db = getDb();
 
   const languages = await db('languages')
     .select('*')
+    .where(filter(filtering))
     .limit(pagination.pageSize)
     .offset(pagination.startIndex)
-    .orderBy('id');
+    .orderBy(sort, direction);
 
   return z.array(LanguageSchema).parse(languages);
 };
 
-const count: ILanguageStore['count'] = async (): Promise<number> => {
+const count: ILanguageStore['count'] = async (
+  filtering: LanguageFilter = {},
+): Promise<number> => {
   const db = getDb();
 
-  const [{ count }] = await db('languages').count();
+  const [{ count }] = await db('languages').where(filter(filtering)).count();
 
   return typeof count === 'string' ? parseInt(count, 10) : count;
 };
