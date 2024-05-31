ad37414c7e4b4b2012d01630d391d5a4f46d7ff1
David Figueroa <david.figueroa@nrc.no>
2024-05-31 10:20:06 +0200

    removed dateOfBirth index for the time being

--- libs/db/migrations/20240529125552_create-participant-indexes-for-sorting.ts
+++ libs/db/migrations/20240529125552_create-participant-indexes-for-sorting.ts
@@ -3,13 +3,11 @@ import type { Knex } from 'knex';
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.table('participants', (table) => {
     table.index('last_name');
-    table.index('date_of_birth');
   });
 }
 
 export async function down(knex: Knex): Promise<void> {
   await knex.schema.table('participants', (table) => {
     table.dropIndex('last_name');
-    table.dropIndex('date_of_birth');
   });
 }
