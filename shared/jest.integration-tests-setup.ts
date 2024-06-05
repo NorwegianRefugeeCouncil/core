458c2d47def2c9f1d48b7030205b141cb1b4731c
David Figueroa <dfigueroa87@users.noreply.github.com>
2024-06-05 13:20:35 +0200

    [CORE24-361] fix(ci): Moved migrations for integration tests to global setup (#91)

--- shared/jest.integration-tests-setup.ts
+++ shared/jest.integration-tests-setup.ts
@@ -1,7 +1,10 @@
 import { config as dotenvConfig } from 'dotenv';
+import * as path from 'path';
 import { DockerComposeEnvironment, Wait } from 'testcontainers';
 import { registerTsProject } from '@nx/js/src/internal';
 const cleanupRegisteredPaths = registerTsProject('./tsconfig.base.json');
+
+import { getDb } from '@nrcno/core-db';
 
 export default async function () {
   // Start services that the app needs to run (e.g. database, docker-compose, etc.).
@@ -20,6 +23,21 @@ export default async function () {
     .withWaitStrategy('db', Wait.forHealthCheck())
     .withStartupTimeout(120 * 1000)
     .up();
+
+  const config = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  };
+  const db = getDb(config);
+  await db.migrate.latest({
+    loadExtensions: ['.js'],
+  });
+  await db.seed.run({
+    loadExtensions: ['.js'],
+    directory: path.join(db.client.config.seeds.directory, 'common'),
+  });
 
   // Pass the environment to global teardown
   (global as any).__ENVIRONMENT__ = environment;
