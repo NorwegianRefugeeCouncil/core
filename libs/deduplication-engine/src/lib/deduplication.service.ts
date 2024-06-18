ddc96b7c7903358b396736c1470a4c62cb4131a2
Ben McAlindin <bmcalindin@gmail.com>
2024-06-18 14:17:44 +0200

    Cleanjp

--- libs/deduplication-engine/src/lib/deduplication.service.ts
+++ libs/deduplication-engine/src/lib/deduplication.service.ts
@@ -44,6 +44,7 @@ interface IDeduplicationService {
   countDuplicates: () => Promise<number>;
 }
 
+// TODO remove when DB is implemented
 // Compare two participants
 const compareParticipants = (
   participantA: Partial<Participant>,
@@ -98,10 +99,13 @@ const compareParticipants = (
   };
 };
 
+// TODO refactor to use DB
 // Used for finding duplicates when inputting a new participant
 const getDuplicatesForParticipant = async (
   participantA: Partial<Participant>,
 ): Promise<DeduplicationRecordDefinition[]> => {
+  throw new Error('Not implemented');
+  /*
   const getDuplicatesForParticipantIdBatch = async (
     startIndex: number,
   ): Promise<DeduplicationRecordDefinition[]> => {
@@ -129,36 +133,7 @@ const getDuplicatesForParticipant = async (
   }
 
   return results.sort((a, b) => b.weightedScore - a.weightedScore);
-};
-
-// Used for the worker to calculate existing duplicates
-const __compareAllParticipants = async (): Promise<void> => {
-  const getDuplicatesForParticipantIdBatch = async (
-    startIndex: number,
-  ): Promise<DeduplicationRecordDefinition[]> => {
-    const participants = await participantService.listFull({
-      startIndex,
-      pageSize: batchSize,
-    });
-    return participants.flatMap((participantA) =>
-      participants.map((participantB) =>
-        compareParticipants(participantA, participantB),
-      ),
-    );
-  };
-
-  const totalParticipants = await participantService.count();
-  const batches = Math.ceil(totalParticipants / batchSize);
-  for (let i = 0; i < batches; i++) {
-    const startIdx = i * batchSize;
-    const records = await getDuplicatesForParticipantIdBatch(startIdx);
-    const results = records.filter(
-      (r) => r.participantIdA !== r.participantIdB && r.weightedScore > cutoff,
-    );
-    if (results.length > 0) {
-      await DeduplicationStore.upsert(results);
-    }
-  }
+  */
 };
 
 const compareAllParticipants = async (fromDate: Date) => {
@@ -257,178 +232,6 @@ const compareAllParticipants = async (fromDate: Date) => {
   }
 };
 
-const ___compareAllParticipants = async (fromDate: Date) => {
-  const logger = getLogger();
-
-  const processDuplicateRecordBatch = async (
-    idx: number,
-    batch: DeduplicationRecordDefinition[],
-  ) => {
-    logger.info(`[Batch ${idx}] Upserting ${batch.length} records...`);
-    await DeduplicationStore.upsert(batch);
-  };
-
-  const processPairBatch = async (
-    idx: number,
-    idPairBatch: [string, string][],
-  ) => {
-    const uniqueIds = new Set<string>(idPairBatch.flat());
-
-    const participants = await participantService.listFullIds(
-      Array.from(uniqueIds),
-    );
-    const participantMap = participants.reduce<Record<string, Participant>>(
-      (acc, p) => ({
-        ...acc,
-        [p.id]: p,
-      }),
-      {},
-    );
-
-    const results = idPairBatch
-      .map(([participantIdA, participantIdB]) => {
-        const participantA = participantMap[participantIdA];
-        const participantB = participantMap[participantIdB];
-        return compareParticipants(participantA, participantB);
-      })
-      .filter(
-        (r): r is DeduplicationRecordDefinition =>
-          r !== null && r.weightedScore > cutoff,
-      );
-
-    logger.info(`[Batch ${idx}] ${results.length}/${idPairBatch.length}`);
-
-    return results;
-  };
-
-  logger.info('Starting deduplication process...');
-
-  logger.info('Preparing deduplication store...');
-  await DeduplicationStore.prepareViews();
-
-  for await (const record of DeduplicationStore.getWeightedMatches(fromDate, {
-    startIndex: 0,
-    pageSize: 1000000000,
-  })) {
-    logger.info(record);
-  }
-
-  logger.info('Fetching exact matches...');
-  let exactIdx = 0;
-  let exactBatch: DeduplicationRecordDefinition[] = [];
-  for await (const exactDuplicate of DeduplicationStore.getExactMatches(
-    fromDate,
-  )) {
-    exactBatch.push(exactDuplicate);
-    if (exactBatch.length >= dbBatchSize) {
-      exactIdx++;
-      await processDuplicateRecordBatch(exactIdx, exactBatch);
-      exactBatch = [];
-    }
-  }
-  if (exactBatch.length > 0) {
-    exactIdx++;
-    await processDuplicateRecordBatch(exactIdx, exactBatch);
-    exactBatch = [];
-  }
-
-  logger.info('Fetching participant id pairs...');
-  let idPairIdx = 0;
-  let weightedIdx = 0;
-  const processedSet = new Set<string>();
-  let idPairBatch: [string, string][] = [];
-  let duplicateRecordBatch: DeduplicationRecordDefinition[] = [];
-  for await (const idPair of DeduplicationStore.getParticipantIdPairs(
-    fromDate,
-  )) {
-    if (idPair[0] === idPair[1]) continue;
-    if (
-      processedSet.has(idPair[0] + idPair[1]) ||
-      processedSet.has(idPair[1] + idPair[0])
-    ) {
-      processedSet.delete(idPair[0] + idPair[1]);
-      processedSet.delete(idPair[1] + idPair[0]);
-      continue;
-    }
-    processedSet.add(idPair[0] + idPair[1]);
-
-    idPairBatch.push(idPair);
-
-    if (idPairBatch.length >= processingBatchSize) {
-      idPairIdx++;
-      const results = await processPairBatch(idPairIdx, idPairBatch);
-      duplicateRecordBatch.push(...results);
-
-      if (duplicateRecordBatch.length >= dbBatchSize) {
-        weightedIdx++;
-        await processDuplicateRecordBatch(weightedIdx, duplicateRecordBatch);
-        duplicateRecordBatch = [];
-      }
-
-      idPairBatch = [];
-    }
-  }
-  if (idPairBatch.length > 0) {
-    idPairIdx++;
-    const results = await processPairBatch(idPairIdx, idPairBatch);
-    duplicateRecordBatch.push(...results);
-    idPairBatch = [];
-  }
-  if (duplicateRecordBatch.length > 0) {
-    weightedIdx++;
-    await processDuplicateRecordBatch(weightedIdx, duplicateRecordBatch);
-    duplicateRecordBatch = [];
-  }
-  logger.info('Deduplication process completed');
-};
-
-// WIP: Fixed version of compareAllParticipants
-const _compareAllParticipants = async () => {
-  const logger = getLogger();
-
-  const totalParticipants = await participantService.count();
-  const batches = Math.ceil(totalParticipants / batchSize);
-
-  const doneSet = new Set<string>();
-
-  for (let i = 0; i < batches; i++) {
-    logger.info(`Processing batch ${i + 1} of ${batches}...`);
-    const outerStartIdx = i * batchSize;
-    const outerParticipants = await participantService.listFull({
-      startIndex: outerStartIdx,
-      pageSize: batchSize,
-    });
-    for (let j = 0; j < batches; j++) {
-      logger.info(`Processing inner batch ${j + 1} of ${batches}...`);
-      const innerStartIdx = j * batchSize;
-      const innerParticipants = await participantService.listFull({
-        startIndex: innerStartIdx,
-        pageSize: batchSize,
-      });
-      for (const participantA of outerParticipants) {
-        const results = innerParticipants
-          .map((participantB) => {
-            if (participantA.id === participantB.id) return null;
-            if (
-              doneSet.has(participantA.id + participantB.id) ||
-              doneSet.has(participantB.id + participantA.id)
-            )
-              return null;
-            doneSet.add(participantA.id + participantB.id);
-            return compareParticipants(participantA, participantB);
-          })
-          .filter(
-            (r): r is DeduplicationRecordDefinition =>
-              r !== null && r.weightedScore > cutoff,
-          );
-        if (results.length > 0) {
-          await DeduplicationStore.upsert(results);
-        }
-      }
-    }
-  }
-};
-
 // Used for resolving duplicates
 const mergeDuplicate = async (
   participantId: string,
@@ -491,6 +294,7 @@ const listDuplicates = async (
   return DeduplicationStore.list(pagination);
 };
 
+// TODO refactor to use DB
 // Used for checking duplicates within a file
 const checkDuplicatesWithinList = async (
   participants: Participant[],
