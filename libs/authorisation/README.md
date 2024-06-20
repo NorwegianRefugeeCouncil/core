b5551990c95379649ff7bed03b59535d1ab12ef3
Ben McAlindin <bmcalindin@gmail.com>
2024-06-20 08:28:58 +0200

    WIP authz lib

---
+++ libs/authorisation/README.md
@@ -0,0 +1,7 @@
+# authorisation-authorisation
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test authorisation-authorisation` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ libs/authorisation/src/lib/authorisation-authorisation.spec.ts
@@ -0,0 +1,7 @@
+import { authorisationAuthorisation } from './authorisation-authorisation';
+
+describe('authorisationAuthorisation', () => {
+  it('should work', () => {
+    expect(authorisationAuthorisation()).toEqual('authorisation-authorisation');
+  });
+});
