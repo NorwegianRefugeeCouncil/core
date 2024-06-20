029bffc6556ff128c3716830546d41da1e8ba308
David Figueroa <dfigueroa87@users.noreply.github.com>
2024-06-20 15:31:12 +0200

    refactor(models): Pick fields from other models (#117)
    * refactor(models): Pick fields from other models
    
    * fix test household generator

--- libs/test-utils/src/lib/prm/household.generator.ts
+++ libs/test-utils/src/lib/prm/household.generator.ts
@@ -28,25 +28,30 @@ const generateEntity = (overrides?: Partial<Household>): Household => {
     ...definition,
     individuals: definition.individuals.map((individual, i) => {
       const phoneId =
-        overrides?.individuals && overrides?.individuals[i].phone?.id;
+        overrides?.individuals && overrides?.individuals[i].phones?.[0]?.id;
       const identificationId =
-        overrides?.individuals && overrides?.individuals[i].identification?.id;
+        overrides?.individuals &&
+        overrides?.individuals[i].identification?.[0].id;
       return {
         ...individual,
         id: faker.string.uuid(),
-        phone: {
-          value: individual.phone?.value || faker.string.alphanumeric(),
-          id: phoneId || faker.string.uuid(),
-        },
-        identification: {
-          identificationNumber:
-            individual.identification?.identificationNumber ||
-            faker.string.uuid(),
-          identificationType:
-            individual.identification?.identificationType ||
-            faker.helpers.enumValue(IdentificationType),
-          id: identificationId || faker.string.uuid(),
-        },
+        phones: [
+          {
+            value: individual.phones?.[0]?.value || faker.string.alphanumeric(),
+            id: phoneId || faker.string.uuid(),
+          },
+        ],
+        identification: [
+          {
+            identificationNumber:
+              individual.identification?.[0]?.identificationNumber ||
+              faker.string.uuid(),
+            identificationType:
+              individual.identification?.[0]?.identificationType ||
+              faker.helpers.enumValue(IdentificationType),
+            id: identificationId || faker.string.uuid(),
+          },
+        ],
       };
     }),
     headNationality: faker.location.countryCode('alpha-3'),
