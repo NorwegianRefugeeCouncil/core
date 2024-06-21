000d9ad763d84e68f62d7b01b235978cc825e224
Ben McAlindin <bmcalindin@gmail.com>
2024-06-21 17:36:12 +0200

    household store integration tests

--- libs/prm-engine/src/lib/stores/household.store.integration.test.ts
+++ libs/prm-engine/src/lib/stores/household.store.integration.test.ts
@@ -1,6 +1,13 @@
+import { over } from 'lodash';
+
 import { HouseholdGenerator } from '@nrcno/core-test-utils';
 import { getDb } from '@nrcno/core-db';
-import { SortingDirection } from '@nrcno/core-models';
+import {
+  HeadOfHouseholdType,
+  Household,
+  HouseholdListSortingFields,
+  SortingDirection,
+} from '@nrcno/core-models';
 
 import { HouseholdStore } from './household.store';
 
@@ -87,21 +94,154 @@ describe('Household store', () => {
       });
     });
 
-    test('should return a paginated list of households, sorted by id by default', async () => {
-      const householdDefinition = HouseholdGenerator.generateDefinition();
-      const household1 = await HouseholdStore.create(householdDefinition);
-      const household2 = await HouseholdStore.create(householdDefinition);
-      const expectedFirstHouseholdId =
-        household1.id < household2.id ? household1.id : household2.id;
+    describe('sorting', () => {
+      test('should return a paginated list of households, sorted by id by default', async () => {
+        const householdDefinition = HouseholdGenerator.generateDefinition();
+        const household1 = await HouseholdStore.create(householdDefinition);
+        const household2 = await HouseholdStore.create(householdDefinition);
+        const expectedFirstHouseholdId =
+          household1.id < household2.id ? household1.id : household2.id;
 
-      const households = await HouseholdStore.list({
-        startIndex: 0,
-        pageSize: 1,
+        const households = await HouseholdStore.list({
+          startIndex: 0,
+          pageSize: 1,
+        });
+
+        expect(households).toBeDefined();
+        expect(households).toHaveLength(1);
+        expect(households[0].id).toEqual(expectedFirstHouseholdId);
       });
 
-      expect(households).toBeDefined();
-      expect(households).toHaveLength(1);
-      expect(households[0].id).toEqual(expectedFirstHouseholdId);
+      const householdListSortingFieldOverrides: Record<string, any> = {
+        id: [{}, {}],
+        headType: [
+          { headType: HeadOfHouseholdType.AdultFemale },
+          { headType: HeadOfHouseholdType.ElderlyMale },
+        ],
+        sizeOverride: [{ sizeOverride: 1 }, { sizeOverride: 2 }],
+      };
+
+      HouseholdListSortingFields.forEach((sortingField) => {
+        Object.entries(SortingDirection).forEach(
+          ([sortingDirection, sortingDirectionValue]) => {
+            test(`should return a paginated list of households, sorted by ${sortingField} ${sortingDirection}`, async () => {
+              const householdDefinition1 =
+                HouseholdGenerator.generateDefinition(
+                  householdListSortingFieldOverrides[sortingField][0],
+                );
+              const householdDefinition2 =
+                HouseholdGenerator.generateDefinition(
+                  householdListSortingFieldOverrides[sortingField][1],
+                );
+
+              const household1 =
+                await HouseholdStore.create(householdDefinition1);
+              const household2 =
+                await HouseholdStore.create(householdDefinition2);
+
+              const expectedFirstHouseholdId =
+                sortingDirectionValue === SortingDirection.Asc
+                  ? household1.id
+                  : household2.id;
+
+              const households = await HouseholdStore.list(
+                {
+                  startIndex: 0,
+                  pageSize: 1,
+                },
+                {
+                  sort: sortingField,
+                  direction: sortingDirectionValue,
+                },
+              );
+
+              expect(households).toBeDefined();
+              expect(households).toHaveLength(1);
+              expect(households[0].id).toEqual(expectedFirstHouseholdId);
+            });
+          },
+        );
+      });
+    });
+
+    describe('filtering', () => {
+      const filteringFields: (
+        | {
+            field: string;
+            overrides: Record<string, any>[];
+            filterValue: any;
+            filterKey?: never;
+          }
+        | {
+            field: string;
+            overrides: Record<string, any>[];
+            filterKey: keyof Household;
+            filterValue?: never;
+          }
+      )[] = [
+        {
+          field: 'id',
+          overrides: [{}, {}],
+          filterKey: 'id',
+        },
+        {
+          field: 'headType',
+          overrides: [
+            { headType: HeadOfHouseholdType.AdultFemale },
+            { headType: HeadOfHouseholdType.ElderlyMale },
+          ],
+          filterKey: 'headType',
+        },
+        {
+          field: 'sizeOverrideMin',
+          overrides: [{ sizeOverride: 3 }, { sizeOverride: 1 }],
+          filterValue: 2,
+        },
+        {
+          field: 'sizeOverrideMax',
+          overrides: [{ sizeOverride: 1 }, { sizeOverride: 3 }],
+          filterValue: 2,
+        },
+      ];
+
+      filteringFields.forEach((filteringField) => {
+        test(`should return a paginated list of households, filtered by ${filteringField}`, async () => {
+          const householdDefinition1 = HouseholdGenerator.generateDefinition(
+            filteringField.overrides[0],
+          );
+          const householdDefinition2 = HouseholdGenerator.generateDefinition(
+            filteringField.overrides[1],
+          );
+
+          const household1 = await HouseholdStore.create(householdDefinition1);
+          await HouseholdStore.create(householdDefinition2);
+
+          const filterValue = (() => {
+            if (filteringField.filterValue) {
+              return filteringField.filterValue;
+            }
+            if (filteringField.filterKey) {
+              return household1[filteringField.filterKey];
+            }
+            throw new Error('Filter value not provided');
+          })();
+
+          const households = await HouseholdStore.list(
+            {
+              startIndex: 0,
+              pageSize: 50,
+            },
+            undefined,
+            {
+              [filteringField.field]: filterValue,
+            },
+          );
+
+          expect(households).toBeDefined();
+          expect(households).toHaveLength(1);
+          expect(households[0].id).toEqual(household1.id);
+        });
+      });
     });
   });
 });
